f5b0b7bb0789137c2ecf66abba3459fe283c932c
Tuaha <your-email@gmail.com>
2026-05-20 19:26:56 +0600

    google auth setup

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -17,8 +17,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENTID,
+      clientSecret: process.env.GOOGLE_SECRET,
     },
   },
 });
